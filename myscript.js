1e62ab369b5050176a4c4cb8cb283e59cece56f8
Francisco Dalla Rosa Soares <dallarosa@gmail.com>
2012-05-31 11:35:06 +0900

    FIXED: changing the text based on the names or classes of the buttons. supporting posts and comments. overlays (picture popups) not supproted yet

--- myscript.js
+++ myscript.js
@@ -1,28 +1,43 @@
 
 $(document).ready(
-function(){
-	$(':button.like_link,:button.stat_elem.as_link.cmnt_like_link').each(function(){
-    if($(this).attr("name") == "like"){
-      $(this).text('ええやん！');
-    }
-    else{
-      $(this).text('ええやん！を取り消す');
-    }
-      });
-
-}
-);
+		function(){
+			$(':button.stat_elem.as_link.cmnt_like_link').each(function(){
+				if($(this).attr("name").indexOf("like_comment_id")==0){
+					$(this).text('ええやん！');
+				}
+				else{
+					$(this).text('ええやん！を取り消す');
+				}
+			});
+			$(':button.like_link,:button.like_link.stat_elem.as_link').each(function(){
+				if($(this).attr("name") == "like"){
+					$(this).text('ええやん！');
+				}
+				else{
+					$(this).text('ええやん！を取り消す');
+				}
+			});
+		}
+		);
 
 $(document).scroll(
-function(){
-	$(':button.like_link').each(function(){
-    if($(this).attr("name") == "like"){
-      $(this).text('ええやん！');
-    }
-    else{
-      $(this).text('ええやん！を取り消す');
-    }
-      });
+		function(){
+			$(':button.stat_elem.as_link.cmnt_like_link').each(function(){
+				if($(this).attr("name").indexOf("like_comment_id")==0){
+					$(this).text('ええやん！');
+				}
+				else{
+					$(this).text('ええやん！を取り消す');
+				}
+			});
+			$(':button.like_link,:button.like_link.stat_elem.as_link').each(function(){
+				if($(this).attr("name") == "like"){
+					$(this).text('ええやん！');
+				}
+				else{
+					$(this).text('ええやん！を取り消す');
+				}
+			});
 
-}
-);
+		}
+		);
