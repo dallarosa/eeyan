9e52d9a395369f7d0b8f89a4ba208dd70ec9e5ec
Yota <www.irda@gmail.com>
2013-08-23 21:21:56 +0900

    FIXED: changing text

--- myscript.js
+++ myscript.js
@@ -17,27 +17,21 @@ $(document).ready(
 					$(this).text('ええやん！を取り消す');
 				}
 			});
+			replaceIineWithEeyan(this);
 		}
 		);
 
-$(document).scroll(
-		function(){
-			$(':button.stat_elem.as_link.cmnt_like_link').each(function(){
-				if($(this).attr("name").indexOf("like_comment_id")==0){
-					$(this).text('ええやん！');
-				}
-				else{
-					$(this).text('ええやん！を取り消す');
-				}
-			});
-			$(':button.like_link,:button.like_link.stat_elem.as_link').each(function(){
-				if($(this).attr("name") == "like"){
-					$(this).text('ええやん！');
-				}
-				else{
-					$(this).text('ええやん！を取り消す');
-				}
-			});
+// 動的に表示されるいいね！にも対応
+$(document).bind('DOMNodeInserted', function(e){
+	var elem = e.target;
+	replaceIineWithEeyan(elem);
+});
 
-		}
-		);
+function replaceIineWithEeyan(elem) {
+	var likeLinkClass = $(elem).find('.UFILikeLink')
+	if(likeLinkClass.length > 0) {
+		var eeneHtml = likeLinkClass.html().replace('いいね', 'ええやん');
+		likeLinkClass.html(eeneHtml);
+	}
+
+};
